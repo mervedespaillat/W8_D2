abd05d4f0447a88bdc9265db55be19d5d769992b
Naoko Sugawara <infoseek122@gmail.com>
2023-04-25 16:42:22 -0400

    updated some enumerable

--- skeleton 4/phase_2_enumerable.js
+++ skeleton 4/phase_2_enumerable.js
@@ -12,35 +12,46 @@ let p = function(n){
     // return n * n;
 }
 
-let example = [1,2,3,4,5]
+// let example = [1,2,3,4,5]
 // example.myEach(p);
 
 
 
 Array.prototype.myMap = function(callback){
     let result = [];
-    for( let i = 0; i < this.length; i++){
-        result.push(callback(this[i]));
-    }
+    // for( let i = 0; i < this.length; i++){
+    //     result.push(callback(this[i]));
+    // }
     return result;
 }
 
-let sum = function(n){
-    return n + n;
-}
+// let sum = function(n){
+//     return n + n;
+// }
 
 let example = [1,2,3,4,5]
 // console.log(example.myMap(sum));
 
 
-Array.prototype.myReduce = function(callback){
+Array.prototype.myReduce = function(callback, acc){
 
-    for( let i = 0; i < this.length; i++){
-        x
-    }
+    // let j = 0;
+    // if (acc === undefined ){
+    //     acc = this[0];
+    //     j = 1;
+    // }
 
+    // for( let i = j; i < this.length; i++){
+    //     acc = callback(this[i], acc)
+
+    // }
+
+
+    return acc;
 }
 
-let sum = function(n){
-    return n + n;
+let sum = function(n, m){
+    return n + m;
 }
+
+console.log(example.myMap(sum, 20));
